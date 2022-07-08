baed4db1549c14e0abee5727021a59303bee906a
Nebula <nebulalol@icloud.com>
2022-07-08 10:15:00 -0400

    IT WORKY

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,67 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { getByProps } from 'enmity/metro';
+import { getByProps, getModule } from 'enmity/metro';
 import { create } from 'enmity/patcher';
-import Manifest from "./manifest.json";
+const MessageStore = getByProps("getMessage", "getMessages");
+import manifest from "./manifest.json";
+const SelectedChannelStore = getByProps("getLastSelectedChannelId");
+const DispatcherModule = getModule((e) => e.dispatch && !e.getCurrentUser);
+const ChannelStore = getByProps("getChannel", "getDMFromUserId");
+import { makeStore } from 'enmity/api/settings';
 
-const Typing = getByProps('startTyping');
+const Patcher = create('lowercase-messages');
 
-const Patcher = create('silent-typing');
-
-const SilentTyping: Plugin = {
-   ...Manifest,
+const LowercaseMessages: Plugin = {
+   ...manifest,
+   patches: [],
 
    onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
+      const Settings = makeStore(this.name);
+
+      Patcher.before(DispatcherModule, "dispatch", (a0, event, a2) => {
+         // Message update event, lowercase the message
+         if (
+            event[0].type === "MESSAGE_UPDATE" &&
+            event[0].message.content !== undefined
+         ) {
+            const selectedGuild = ChannelStore.getChannel(
+               SelectedChannelStore.getChannelId()
+            ).guild_id;
+            if (event[0].message.guild_id !== selectedGuild) return;
+            if (event[0].message.author.id === "721692862521278515") {
+               const originalMessage = MessageStore.getMessage(
+                  event[0].message.channel_id,
+                  event[0].message.id
+               );
+               if (event[0].message.content.slice(-1) === '.') {
+                  event[0].message.content = event[0].message.content.toLowerCase().replace(/.$/,"");
+               }
+               event[0].message.content = event[0].message.content.toLowerCase();
+            }
+            return;
+         }
+
+         // Message create event, lowercase the message
+         if (
+            event[0].type === "MESSAGE_CREATE" &&
+            event[0].message.content !== undefined
+         ) {
+            const selectedGuild = ChannelStore.getChannel(
+               SelectedChannelStore.getChannelId()
+            ).guild_id;
+            if (event[0].message.guild_id !== selectedGuild) return;
+            if (event[0].message.author.id === "721692862521278515") {
+               const originalMessage = MessageStore.getMessage(
+                  event[0].message.channel_id,
+                  event[0].message.id
+               );
+               if (event[0].message.content.slice(-1) === '.') {
+                  event[0].message.content = event[0].message.content.toLowerCase().replace(/.$/,"");
+               }
+               event[0].message.content = event[0].message.content.toLowerCase();
+            }
+            return;
+         }
+      });
    },
 
    onStop() {
@@ -20,4 +69,4 @@ const SilentTyping: Plugin = {
    }
 };
 
-registerPlugin(SilentTyping);
+registerPlugin(LowercaseMessages);
